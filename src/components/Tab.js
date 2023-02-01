cef5cdfece10081d19e7e430a38d0b4ef0d711c2
Gytis <101097792+LeoGytis@users.noreply.github.com>
2023-02-01 08:48:25 +0200

    the begining for today

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -6,16 +6,18 @@ const Tab = () => {
   console.log(HeaderTabs);
   return (
     <Container>
-      Kas negerai?
       {HeaderTabs.map((tab, i) => {
-        console.log(tab);
+        return (
+          <TabContainer key={`tabcontainer-${i}`}>
+            <Icon></Icon>
 
-        <>
-          <Header key={`tabheader-${i}`}>{tab.header}</Header>
-          <Description>{tab.description}</Description>
-        </>;
+            <>
+              <Header>{tab.header}</Header>
+              <Description>{tab.description}</Description>
+            </>
+          </TabContainer>
+        );
       })}
-      <Item></Item>
     </Container>
   );
 };
@@ -23,12 +25,22 @@ const Tab = () => {
 export default Tab;
 
 const Container = styled.div`
-  width: 80%;
+  width: 100%;
   height: 50;
+  display: flex;
+  justify-content: space-evenly;
   border: 2px solid skyblue;
 `;
 
-const Item = styled.div`
+const TabContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  background: #171f32;
+  margin: 10px;
+`;
+
+const Icon = styled.div`
   font-size: 22px;
 `;
 
@@ -36,8 +48,12 @@ const Header = styled.div`
   display: flex;
   width: 100%;
   font-size: 22px;
-  border: 2px solid yellowgreen;
+  font-weight: 700;
+  margin-bottom: 5px;
 `;
+
 const Description = styled.div`
-  font-size: 22px;
+  font-size: 16px;
+  font-weight: 400;
+  margin: 0;
 `;
